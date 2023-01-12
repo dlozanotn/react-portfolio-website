3cead75e3a5ef3063ffed70121b9adb21685fd9b
dlozano <dlozanotn@gmail.com>
2023-01-12 10:12:13 -0600

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/David-Lozano,-Web-Designer-.png'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header_container">
+          <h5>Hello i am</h5>
+          <h1>David Lozano</h1>
+          <h5 className="text-light">Frontend Wordpress Developer and SEO</h5>
+          <CTA />
+          <div className="me">
+              <img src={ME} alt="" />
+          </div>
+
+          <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
