9dd47fb2b411a0617d3c0e16ce7bb56471f0049e
dlozano <dlozanotn@gmail.com>
2023-01-13 16:29:50 -0600

    nav oj¿k

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -12,11 +12,11 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-        <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
-        <a href="#about"><AiOutlineUser /></a>
-        <a href="#experience"><BiBook /></a>
-        <a href="#services"><RiServiceLine /></a>
-        <a href="#contact"><BiMessageRoundedDetail /></a>
+        <a href="#" onClick={() => setActiveNav('#') } className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+        <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#esperience' ? 'active' : ''}><BiBook /></a>
+        <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageRoundedDetail /></a>
     </nav>
   )
 }
