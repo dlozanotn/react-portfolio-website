2494d5b87a0cbd5dd8996e10993404012325e6e6
dlozano <dlozanotn@gmail.com>
2023-01-13 16:43:23 -0600

    about start

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './about.css'
+import Me from '../../assets/David.jpg'
 
 const about = () => {
   return (
-    <section id='about'>about</section>
+    <section id='about'>
+        <h5>Get to Know</h5>
+        <h2>About Me</h2>
+
+        <div className="container about_container">
+            <div className="about_me">
+                  <div className="about_me-image">
+                    <img src= {Me} alt="About Img" />
+                  </div>
+            </div>
+
+            <div className="about_content">
+                <div className="about_cards">
+                    <article className='about_card'>
+                         <h5>Experience</h5> 
+                         <small>3+ Years Working</small>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
