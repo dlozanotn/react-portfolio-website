1bae4cb16fb9b6881347f436390babbe57913520
dlozano <dlozanotn@gmail.com>
2023-01-14 17:14:55 -0600

    about ok

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import Me from '../../assets/David.jpg'
+import {FaAward} from 'react-icons/fa' 
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const about = () => {
   return (
@@ -18,10 +21,30 @@ const about = () => {
             <div className="about_content">
                 <div className="about_cards">
                     <article className='about_card'>
+                         <FaAward className='about_icon'/>
                          <h5>Experience</h5> 
                          <small>3+ Years Working</small>
                     </article>
+
+                    <article className='about_card'>
+                         <FiUsers className='about_icon' />
+                         <h5>Clents</h5> 
+                         <small>50+ Worldwide</small>
+                    </article>
+
+                    <article className='about_card'>
+                         <VscFolderLibrary className='about_icon' />
+                         <h5>Projects</h5> 
+                         <small>20+ completed</small>
+                    </article>
                 </div>
+
+                <p>
+                  MY NAME IS DAVID LOZANO TENORIO, I AM A WEB DESIGN PROFESSIONAL, CREATIVE AND RESPONSIBLE, WITH THE PLEASURE OF BEING IN CONSTANT LEARNING AND UPDATES. I HAVE LEARNED HOW TO USE WORDPRESS THROUGH DIFFERENT COURSES, AND I SPECIALIZE IN THE ELEMENTOR BUILDER. I HAVE DESIGNED DIFFERENT WEBSITES EITHER FROM 0, MODIFYING THEMES, OR MODIFYING CODE.I AM CURRENTLY TAKING COURSES TO HAVE AN ADVANCED LEVEL IN JS AND PHP, AS THE CAREER PATH FULLSTACK ENGINEER IN CODEACADEMY. I CAN ALSO PERFORM WORDPRESS MIGRATIONS WITHOUT THE PLUGIN, MODIFY THE INI.PHP FILE ACCORDING TO THE NEEDS OF A THEME, AND CREATE WEBSITES FROM DESIGNS FROM ADOBE XD, PNG, OR PHOTOSHOP. I ALSO DO DESIGN IN PHOTOSHOP AND ILLUSTRATOR AND THIS LETTER WAS MADE IN INDESIGN, AS WELL AS VIDEO EDITING WITH DAVINCI AND AFTER EFFECTS SOFTWARE, ALSO AUDIO EDITING.
+                  THESE SKILLS ALLOW ME TO DEVELOP AUTONOMOUSLY IN CREATING A WEBSITE SINCE I DO FROM DESIGN, IMAGES, VIDEO, AUDIO, AND WRITING. IN ADDITION TO SEO CONFIGURATION AND SEO OPTIMIZATION TO CLIMB IN SEARCH ENGINES ORGANICALLY.
+                </p>
+
+                <a href="#contact" className='btn btn-primary'>Let´s Talk</a>
             </div>
         </div>
     </section>
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,9 +1,102 @@
 import React from 'react'
 import './experience.css'
+import {FaHtml5} from 'react-icons/fa'
+import {FaCss3Alt} from 'react-icons/fa'
+import {FaBootstrap} from 'react-icons/fa'
+import {FaWordpress} from 'react-icons/fa'
+import {SiJavascript} from 'react-icons/si'
+import {FaReact} from 'react-icons/fa'
+import {FaFigma} from 'react-icons/fa'
 
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>What Skills i Have</h5>
+      <h2>My experience</h2>
+
+      <div className="container experience_container">
+        <div className="experience_frontend">
+              <h3>Frontend Development</h3>
+              <div className="experience_content">
+                  <article className='experience_detail'>
+                      <FaHtml5 />
+                      <h4>HTML</h4>
+                      <small className='text-light'>Experienced</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaCss3Alt />
+                      <h4>CSS</h4>
+                      <small className='text-light'>Intermediate</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaBootstrap />
+                      <h4>bootstrap</h4>
+                      <small className='text-light'>Experienced</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaWordpress />
+                      <h4>Wordpress</h4>
+                      <small className='text-light'>Experienced</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <SiJavascript />
+                      <h4>javascript</h4>
+                      <small className='text-light'>basic</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaReact />
+                      <h4>React</h4>
+                      <small className='text-light'>basic</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaFigma />
+                      <h4>Figma</h4>
+                      <small className='text-light'>Intermediate</small>
+                  </article>
+              </div>
+        </div>
+        <div className="experience_backend">
+        <h3>Backend Development</h3>
+              <div className="experience_content">
+                  <article className='experience_detail'>
+                      <FaHtml5 />
+                      <h4>HTML</h4>
+                      <small className='text-light'>Experienced</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaCss3Alt />
+                      <h4>CSS</h4>
+                      <small className='text-light'>Intermediate</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaBootstrap />
+                      <h4>bootstrap</h4>
+                      <small className='text-light'>Experienced</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaWordpress />
+                      <h4>Wordpress</h4>
+                      <small className='text-light'>Experienced</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <SiJavascript />
+                      <h4>javascript</h4>
+                      <small className='text-light'>basic</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaReact />
+                      <h4>React</h4>
+                      <small className='text-light'>basic</small>
+                  </article>
+                  <article className='experience_detail'>
+                      <FaFigma />
+                      <h4>Figma</h4>
+                      <small className='text-light'>Intermediate</small>
+                  </article>
+              </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
